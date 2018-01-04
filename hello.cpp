0d3cfed8818d756d177fde87191d391692f22164
Jerry <jerryhou0129@gmail.com>
2018-01-04 18:27:28 +0800

    create a git project

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main()
+
+{
+    std:count << "Hello Git!" << std:endl;
+    return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is my first git and githun and vi test conducted on my Ubuntu
